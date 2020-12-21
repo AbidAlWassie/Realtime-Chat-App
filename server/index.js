680773ffed4d565241144737f07e17d4cdee539e
Abid Al-Wasi <abidalwasi@outlook.com>
2020-12-21 23:29:00 +0600

    updated project

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));

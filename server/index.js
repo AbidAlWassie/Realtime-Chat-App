f16e96249aecd71880a8dfe6250a23655044a39e
Abid Al-Wasi <abidalwasi@outlook.com>
2020-12-22 03:19:26 +0600

    updated project

--- server/index.js
+++ server/index.js
@@ -4,8 +4,21 @@ const http = require("http");
 
 const PORT = process.env.PORT || 8080;
 
+const router = require("./router");
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on("connection", () => {
+  console.log("New user joined");
+  
+  socket.on("disconnect", () => {
+    console.log("User has left")
+  });
+});
+
+app.use(router);
+
 
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
